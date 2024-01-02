db3c6ee441589527132cf321c46b4b67495b8454
Adrienne Ortiz <ani650@gmail.com>
2024-01-03 06:22:10 +0800

    Add code  task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerID = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+startBtn.addEventListener('click', onStart);
+
+function onStop() {
+  clearInterval(timerID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+stopBtn.addEventListener('click', onStop);
